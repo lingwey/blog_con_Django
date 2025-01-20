0bfb6dfd3b51ebb5cfb80d1e864c3c56f0eeceff
lingwey <nachozanetti0@gmail.com>
2025-01-20 16:40:59 -0300

    agrega el modelo de la base de datos y el admin

--- blog/app_blog/admin.py
+++ blog/app_blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
+#admin.site.register(Post)
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'slug','autor', 'publicacion', 'status']
+    list_filter = ['status', 'creacion', 'publicacion', 'autor']
+    search_fields = ['titulo','cuerpo']
+    prepopulated_fields= {'slug': ('titulo',)}
+    raw_id_fields = ['autor']
+    date_hierarchy = 'publicacion'
+    ordering= ['status', 'publicacion']
--- blog/app_blog/models.py
+++ blog/app_blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post (models.Model):
+    
+    class Status (models.TextChoices):
+        BORRADOR = 'df', 'borrador'
+        PUBLICO = 'pb', 'publico'
+        
+    titulo = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
+    cuerpo = models.TextField()
+    publicacion = models.DateTimeField(default=timezone.now)
+    creacion = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.BORRADOR)
+    
+    class Meta:
+        ordering =['-publicacion']
+        indexes = [models.Index(fields=['-publicacion'])]
+    
+    def __str__(self):
+        return self.titulo
