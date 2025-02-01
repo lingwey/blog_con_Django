68e72553705c02b6b7449172a5f188ef0d81e3b7
lingwey <nachozanetti0@gmail.com>
2025-02-01 00:31:35 -0300

    soluciona el bug del filtrado por tags

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_HOST_USER='nachozanetti0@gmail.com'
-EMAIL_HOST_PASSWORD='bqwf bnub ohxt bgqo'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAIL_PORT=587
 EMAIL_USE_TLS= True
